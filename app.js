9a8ffe0e099847b070efd3dd0e756d3add59210d
YawHB <yawhboateng@gmail.com>
2023-04-06 11:05:29 +0200

    textcontent on h2 and signs are working

--- app.js
+++ app.js
@@ -1,39 +1,17 @@
 /***********************************************************/
 
-//Determines the computers pick
-function computerPlaying() {
-  const computerWeapon = Math.trunc(Math.random() * 3);
-  if (computerWeapon === 0) {
-    return "Rock";
-  } else if (computerWeapon === 1) {
-    return "Paper";
-  } else if (computerWeapon === 2) {
-    return "Scissor";
-  }
-}
+let playerScore = 0;
+let computerScore = 0;
 
-//Register the players choice
-function playerChoice() {
-  const word = prompt("Choose Rock, Paper or Scissor");
-  const firstLetter = word[0].toUpperCase();
-  const restOfWord = word.substring(1).toLowerCase();
-  const playerWeapon = firstLetter + restOfWord;
-  if (
-    playerWeapon === "Rock" ||
-    playerWeapon === "Paper" ||
-    playerWeapon === "Scissor"
-  ) {
-    return playerWeapon;
-  } else {
-    console.log("invalid");
-  }
-}
+document.querySelector(".rock-box").addEventListener("click", playGame);
+document.querySelector(".paper-box").addEventListener("click", playGame);
+document.querySelector(".scissors-box").addEventListener("click", playGame);
 
 function playGame() {
-  let playerScore = 0;
-  let computerScore = 0;
+  let playerWeapon = this;
+  console.log(playerWeapon);
   for (let i = 0; i < 5; i++) {
-    const playerSelection = playerChoice();
+    const playerSelection = playerChoice(this);
     const computerSelection = computerPlaying();
     const result = winnerOfRound(playerSelection, computerSelection);
 
@@ -69,19 +47,127 @@ function playGame() {
   }
 }
 
-function winnerOfRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
-    return "tie";
-  } else if (
-    (playerSelection === "Rock" && computerSelection === "Scissor") ||
-    (playerSelection === "Scissor" && computerSelection === "Paper") ||
-    (playerSelection === "Paper" && computerSelection === "Rock")
-  ) {
-    return "you won";
-  } else {
-    return "computer won";
+//Determines the computers pick
+function computerPlaying() {
+  const computerWeapon = Math.trunc(Math.random() * 3);
+  if (computerWeapon === 0) {
+    return document.querySelector(".rock-box");
+  } else if (computerWeapon === 1) {
+    return document.querySelector(".paper-box");
+  } else if (computerWeapon === 2) {
+    return document.querySelector(".scissors-box");
   }
 }
+
+//Register the players choice
+function playerChoice(playerChoice) {
+  console.log(playerChoice);
+  // const word = prompt("Choose Rock, Paper or Scissor");
+  // const firstLetter = word[0].toUpperCase();
+  // const restOfWord = word.substring(1).toLowerCase();
+  // const playerWeapon = firstLetter + restOfWord;
+
+  playerChoice === document.querySelector(".rock-box") ||
+    playerChoice === document.querySelector(".paper-box") ||
+    playerChoice === document.querySelector(".scissors-box");
+
+  console.log(playerChoice);
+  return playerChoice;
+}
+
+function winnerOfRound(playerSelection, computerSelection) {
+  console.log(playerSelection); // DOM element
+  console.log(computerSelection); // DOM element
+  displayPlayerEmojiResult(playerSelection);
+  displayComputerEmojiResult(computerSelection);
+  console.log(playerSelection);
+  console.log(computerSelection);
+
+  displayTextResults(playerSelection, computerSelection);
+
+  displayPlayerEmojiResult(playerSelection);
+  displayComputerEmojiResult(computerSelection);
+}
+
+function displayTextResults(playerSelection, computerSelection) {
+  const rock = document.querySelector(".rock-box");
+  const paper = document.querySelector(".paper-box");
+  const scissors = document.querySelector(".scissors-box");
+  if (playerSelection === computerSelection) {
+    document.querySelector(".round-result").textContent = `It's a tie!`;
+    document.querySelector(
+      ".round-result-reason"
+    ).textContent = `${playerSelection} ties with ${computerSelection}`;
+  } else if (
+    (rock && document.querySelector(".scissors-box")) ||
+    (paper && document.querySelector(".rock-box")) ||
+    (scissors && document.querySelector(".paper-box"))
+  ) {
+    document.querySelector(".round-result").textContent = `You won this round`;
+    document.querySelector(
+      ".round-result-reason"
+    ).textContent = `${playerSelection} beats ${computerSelection}`;
+  } else if (
+    (rock && document.querySelector(".paper-box")) ||
+    (scissors && document.querySelector(".rock-box")) ||
+    (paper && document.querySelector(".scissors-box"))
+  ) {
+    document.querySelector(".round-result").textContent = `You lost this round`;
+    document.querySelector(
+      ".round-result-reason"
+    ).textContent = `${computerSelection} beats ${playerSelection}`;
+  }
+
+  /***********CALL DISPLAY EMOJI RESULTS HERE with two args: playerSelection and computerSelection*********** */
+}
+
+/***********NAME THIS DISPLAY EMOJI RESULTS INSTEAD takes two arguments*********** */
+function displayPlayerEmojiResult(playerSelection) {
+  /***********IF Rock === ROCK then #player_weapon AND computerWeapon.textcontent = "🪨"*********** */
+  console.log("Enter: getEmojiResult");
+
+  const rock = document.querySelector(".rock-box");
+  const paper = document.querySelector(".paper-box");
+  const scissors = document.querySelector(".scissors-box");
+
+  const playerWeapon = document.querySelector("#player-weapon");
+  if (rock === playerSelection) {
+    playerWeapon.textContent = "🪨";
+  } else if (paper === playerSelection) {
+    playerWeapon.textContent = "📄";
+  } else if (scissors === playerSelection) {
+    playerWeapon.textContent = "✂️";
+  }
+}
+
+function displayComputerEmojiResult(computerSelection) {
+  const rock = document.querySelector(".rock-box");
+  const paper = document.querySelector(".paper-box");
+  const scissors = document.querySelector(".scissors-box");
+  const computerWeapon = document.querySelector("#computer-weapon");
+
+  if (rock === computerSelection) {
+    computerWeapon.textContent = "🪨";
+  } else if (paper === computerSelection) {
+    computerWeapon.textContent = "📄";
+  } else if (scissors === computerSelection) {
+    computerWeapon.textContent = "✂️";
+  }
+}
+
+// function displayEmojiResult(pick) {
+//   const rock = document.querySelector(".rock-box");
+//   const paper = document.querySelector(".paper-box");
+//   const scissors = document.querySelector(".scissors-box");
+
+//   if (pick === rock) {
+//     document.querySelector("#player-weapon").textContent = `🪨`;
+//   } else if (pick === paper) {
+//     document.querySelector("#player-weapon").textContent = `📄`;
+//   } else if (pick === scissors) {
+//     document.querySelector("#player-weapon").textContent = `✂️`;
+//   }
+// }
 
 // Announces that player has won
 function playerWon(you, computer) {
@@ -102,5 +188,3 @@ function gameTie(you, computer) {
   You: ${you}
   Computer: ${computer}`;
 }
-
-playGame();
