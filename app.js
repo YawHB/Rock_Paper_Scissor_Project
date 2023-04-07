b9bf17cb50de69c3f065a9736d09bcef32245307
YawHB <yawhboateng@gmail.com>
2023-04-07 15:31:23 +0200

    Project complete
    There may be room for some minor CSS adjustments, but the game is now ready to be played.
    
    I may want to remove the background color for the player/computer when he/it looses, so the screen is only green or red respectively.

--- app.js
+++ app.js
@@ -1,52 +1,88 @@
-/***********************************************************/
+"use strict";
 
-let playerScore = 0;
-let computerScore = 0;
+window.addEventListener("load", initApp);
 
-document.querySelector(".rock-box").addEventListener("click", playGame);
-document.querySelector(".paper-box").addEventListener("click", playGame);
-document.querySelector(".scissors-box").addEventListener("click", playGame);
+let playerPoints = 0;
+let computerPoints = 0;
+console.log("player points: " + playerPoints);
+
+function initApp() {
+  document.querySelector(".rock-box").addEventListener("click", playGame);
+  document.querySelector(".paper-box").addEventListener("click", playGame);
+  document.querySelector(".scissors-box").addEventListener("click", playGame);
+  document
+    .querySelector("#btn-play-again")
+    .addEventListener("click", restartGame);
+}
 
 function playGame() {
-  let playerWeapon = this;
-  console.log(playerWeapon);
-  for (let i = 0; i < 5; i++) {
-    const playerSelection = playerChoice(this);
-    const computerSelection = computerPlaying();
-    const result = winnerOfRound(playerSelection, computerSelection);
+  document.querySelector("h2").textContent = " ";
+  const playerSelection = playerChoice(this);
+  const computerSelection = computerPlaying();
 
-    if (result === "you won") {
-      playerScore++;
-      console.log(`You won round ${i + 1} of 5
-      The score is 
-      you: ${playerScore} (${playerSelection})
-      Computer: ${computerScore} (${computerSelection})`);
-    } else if (result === "computer won") {
-      computerScore++;
-      console.log(`Computer won round ${i + 1} of 5
-      The score is 
-      you: ${playerScore} (${playerSelection})
-      Computer: ${computerScore} (${computerSelection})`);
-    } else if (result === "tie") {
-      console.log(`Round ${i + 1} was tied.
-      The score is 
-      you: ${playerScore} (${playerSelection})
-      Computer: ${computerScore} (${computerSelection})`);
-    }
+  /*const result = */ winnerOfRound(playerSelection, computerSelection);
+}
+
+function incrementPlayerPoint() {
+  playerPoints++;
+  console.log("increment player points: " + playerPoints);
+  displayPlayerPoints();
+  if (playerPoints >= 2) {
+    youWin();
   }
-
-  if (playerScore > computerScore) {
-    let finalScore = playerWon(playerScore, computerScore);
-    console.log(finalScore);
-  } else if (computerScore > playerScore) {
-    finalScore = computerWon(playerScore, computerScore);
-    console.log(finalScore);
-  } else {
-    finalScore = gameTie(playerScore, computerScore);
-    console.log(finalScore);
+}
+function incrementcomputerPoint() {
+  computerPoints++;
+  displaycomputerPoints();
+  if (computerPoints >= 2) {
+    computerWin();
   }
 }
 
+function displayPlayerPoints() {
+  document.querySelector("#display-player-score").textContent = playerPoints;
+}
+
+function displaycomputerPoints() {
+  document.querySelector("#display-computer-score").textContent =
+    computerPoints;
+}
+
+function youWin() {
+  document.querySelector(".rock-box").removeEventListener("click", playGame);
+  document.querySelector(".paper-box").removeEventListener("click", playGame);
+  document
+    .querySelector(".scissors-box")
+    .removeEventListener("click", playGame);
+  winningBackgroundColor();
+  document.querySelector("#sound-game-won").play();
+  document.querySelector("#sound-game-won").currentTime = 0;
+  document.querySelector("#player-result").style.backgroundColor = "#60b347";
+  document.querySelector("#computer-result").style.backgroundColor = "#60b347";
+}
+
+function winningBackgroundColor() {
+  document.querySelector("body").style.backgroundColor = "#60b347";
+  document.querySelector("#player-result").style.backgroundColor = "#60b347";
+  document.querySelector("#computer-result").style.backgroundColor = "#60b347";
+}
+
+function computerWin() {
+  document.querySelector(".rock-box").removeEventListener("click", playGame);
+  document.querySelector(".paper-box").removeEventListener("click", playGame);
+  document
+    .querySelector(".scissors-box")
+    .removeEventListener("click", playGame);
+  loosingBackgroundColor();
+  document.querySelector("#sound-game-lost").play();
+  document.querySelector("#sound-game-lost").currentTime = 0;
+  document.querySelector("#computer-result").style.backgroundColor = "#EF0107";
+}
+
+function loosingBackgroundColor() {
+  document.body.style.backgroundColor = "#EF0107";
+  document.querySelector("#player-result").style.backgroundColor = "#EF0107";
+}
 //Determines the computers pick
 function computerPlaying() {
   const computerWeapon = Math.trunc(Math.random() * 3);
@@ -61,130 +97,106 @@ function computerPlaying() {
 
 //Register the players choice
 function playerChoice(playerChoice) {
-  console.log(playerChoice);
-  // const word = prompt("Choose Rock, Paper or Scissor");
-  // const firstLetter = word[0].toUpperCase();
-  // const restOfWord = word.substring(1).toLowerCase();
-  // const playerWeapon = firstLetter + restOfWord;
-
   playerChoice === document.querySelector(".rock-box") ||
     playerChoice === document.querySelector(".paper-box") ||
     playerChoice === document.querySelector(".scissors-box");
 
-  console.log(playerChoice);
   return playerChoice;
 }
 
 function winnerOfRound(playerSelection, computerSelection) {
-  console.log(playerSelection); // DOM element
-  console.log(computerSelection); // DOM element
-  displayPlayerEmojiResult(playerSelection);
-  displayComputerEmojiResult(computerSelection);
-  console.log(playerSelection);
-  console.log(computerSelection);
-
-  displayTextResults(playerSelection, computerSelection);
-
-  displayPlayerEmojiResult(playerSelection);
-  displayComputerEmojiResult(computerSelection);
+  const playerEmoji = displayPlayerEmojiResult(playerSelection);
+  const computerEmoji = displayComputerEmojiResult(computerSelection);
+  versus(playerEmoji, computerEmoji);
 }
 
-function displayTextResults(playerSelection, computerSelection) {
-  const rock = document.querySelector(".rock-box");
-  const paper = document.querySelector(".paper-box");
-  const scissors = document.querySelector(".scissors-box");
-  if (playerSelection === computerSelection) {
-    document.querySelector(".round-result").textContent = `It's a tie!`;
-    document.querySelector(
-      ".round-result-reason"
-    ).textContent = `${playerSelection} ties with ${computerSelection}`;
+function versus(playerEmoji, computerEmoji) {
+  const rock = "🪨";
+  const paper = "📄";
+  const scissors = "✂️";
+
+  if (playerEmoji === computerEmoji) {
+    document.querySelector("#vs").textContent = "ties with";
+    tiedColor();
   } else if (
-    (rock && document.querySelector(".scissors-box")) ||
-    (paper && document.querySelector(".rock-box")) ||
-    (scissors && document.querySelector(".paper-box"))
+    (playerEmoji === rock && computerEmoji === scissors) ||
+    (playerEmoji === paper && computerEmoji === rock) ||
+    (playerEmoji === scissors && computerEmoji === paper)
   ) {
-    document.querySelector(".round-result").textContent = `You won this round`;
-    document.querySelector(
-      ".round-result-reason"
-    ).textContent = `${playerSelection} beats ${computerSelection}`;
-  } else if (
-    (rock && document.querySelector(".paper-box")) ||
-    (scissors && document.querySelector(".rock-box")) ||
-    (paper && document.querySelector(".scissors-box"))
-  ) {
-    document.querySelector(".round-result").textContent = `You lost this round`;
-    document.querySelector(
-      ".round-result-reason"
-    ).textContent = `${computerSelection} beats ${playerSelection}`;
+    document.querySelector("#vs").textContent = "beats";
+    incrementPlayerPoint();
+    playerColor();
+  } else {
+    document.querySelector("#vs").textContent = "is beating by";
+    incrementcomputerPoint();
+    computerColor();
   }
-
-  /***********CALL DISPLAY EMOJI RESULTS HERE with two args: playerSelection and computerSelection*********** */
 }
 
-/***********NAME THIS DISPLAY EMOJI RESULTS INSTEAD takes two arguments*********** */
+function tiedColor() {
+  document.querySelector("#player-result").style.backgroundColor = "#034694";
+  document.querySelector("#computer-result").style.backgroundColor = "#034694";
+}
+
+function playerColor() {
+  document.querySelector("#player-result").style.backgroundColor = "#60b347";
+  document.querySelector("#computer-result").style.backgroundColor = "#EF0107";
+}
+
+function computerColor() {
+  document.querySelector("#computer-result").style.backgroundColor = "#60b347";
+  document.querySelector("#player-result").style.backgroundColor = "#EF0107";
+}
+// Shows players weapon 🪨📄✂️
 function displayPlayerEmojiResult(playerSelection) {
-  /***********IF Rock === ROCK then #player_weapon AND computerWeapon.textcontent = "🪨"*********** */
-  console.log("Enter: getEmojiResult");
-
   const rock = document.querySelector(".rock-box");
   const paper = document.querySelector(".paper-box");
   const scissors = document.querySelector(".scissors-box");
 
-  const playerWeapon = document.querySelector("#player-weapon");
+  const playerWeapon = document.querySelector("#player-result");
   if (rock === playerSelection) {
-    playerWeapon.textContent = "🪨";
+    return (playerWeapon.textContent = "🪨");
   } else if (paper === playerSelection) {
-    playerWeapon.textContent = "📄";
+    return (playerWeapon.textContent = "📄");
   } else if (scissors === playerSelection) {
-    playerWeapon.textContent = "✂️";
+    return (playerWeapon.textContent = "✂️");
   }
 }
 
+// Shows computer weapon 🪨📄✂️
 function displayComputerEmojiResult(computerSelection) {
   const rock = document.querySelector(".rock-box");
   const paper = document.querySelector(".paper-box");
   const scissors = document.querySelector(".scissors-box");
-  const computerWeapon = document.querySelector("#computer-weapon");
+  const computerWeapon = document.querySelector("#computer-result");
 
   if (rock === computerSelection) {
-    computerWeapon.textContent = "🪨";
+    return (computerWeapon.textContent = "🪨");
   } else if (paper === computerSelection) {
-    computerWeapon.textContent = "📄";
+    return (computerWeapon.textContent = "📄");
   } else if (scissors === computerSelection) {
-    computerWeapon.textContent = "✂️";
+    return (computerWeapon.textContent = "✂️");
   }
 }
 
-// function displayEmojiResult(pick) {
-//   const rock = document.querySelector(".rock-box");
-//   const paper = document.querySelector(".paper-box");
-//   const scissors = document.querySelector(".scissors-box");
+function restartGame() {
+  console.log("button");
+  playerPoints = 0;
+  computerPoints = 0;
+  document.querySelector("#display-player-score").textContent = 0;
+  document.querySelector("#display-computer-score").textContent = 0;
 
-//   if (pick === rock) {
-//     document.querySelector("#player-weapon").textContent = `🪨`;
-//   } else if (pick === paper) {
-//     document.querySelector("#player-weapon").textContent = `📄`;
-//   } else if (pick === scissors) {
-//     document.querySelector("#player-weapon").textContent = `✂️`;
-//   }
-// }
+  document.querySelector(".rock-box").addEventListener("click", playGame);
+  document.querySelector(".paper-box").addEventListener("click", playGame);
+  document.querySelector(".scissors-box").addEventListener("click", playGame);
 
-// Announces that player has won
-function playerWon(you, computer) {
-  return `Congratulations - YOU WON!! The final score is
-  You: ${you}
-  Computer: ${computer}`;
-}
+  document.querySelector("#player-result").style.backgroundColor = "";
+  document.querySelector("#computer-result").style.backgroundColor = "";
+  document.querySelector("body").style.backgroundColor = "";
 
-// Announces that computer has won
-function computerWon(you, computer) {
-  return `Better Luck next time - YOU LOST!! The final score is
-  You: ${you}
-  Computer: ${computer}`;
-}
+  document.querySelector("h2").textContent = "GET READY";
 
-function gameTie(you, computer) {
-  return `Close but no cigar - IT'S A TIE!! The final score is
-  You: ${you}
-  Computer: ${computer}`;
+  // document.querySelector("#player-result").textContent = "？";
+  // document.querySelector("#computer-result").textContent = "？";
+  document.querySelector("#vs").textContent = "";
 }
